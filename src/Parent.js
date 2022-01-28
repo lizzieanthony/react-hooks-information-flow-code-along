5422e1bdabf28c5c2d501684aa9810db44406ec7
Lizzie <lizziewrab@me.com>
2022-01-28 14:14:28 -0600

    commit tree 1dc2ef4a2c57376203609e9929a8d0226ee4c40e

--- src/Parent.js
+++ src/Parent.js
@@ -5,13 +5,38 @@ import Child from "./Child";
 function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
+  const [childrenColor, setChildrenColor] = useState("#FFF");
+  
+
+  function handleChangeColor(newChildColor) {
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor); 
+    setChildrenColor(newChildColor);
+  }
 
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
     </div>
   );
 }
 
 export default Parent;
+
+
+ // const [mood, setMood] = useState('chill');
+
+  // function handleGettingToWork() {
+  //   setMood('stressed')
+  // }
+  // function handleClockOut(){
+  //   setMood('relieved')
+  // }
+
+  // return (
+  // <div>
+  //   <p>i am {mood}</p> 
+  //   <button onClick={handleGettingToWork}>clock in</button> 
+  //   <button onClick={handleClockOut}>clock out</button>
+  // </div>)
